7d1478bfa8a5937b29aa8a4ad20805e63e888715
Den163 <abramov.denis163@gmail.com>
2023-05-26 22:50:13 +0400

    Added gcc linux support

--- build.rs
+++ build.rs
@@ -3,6 +3,14 @@ use std::path::Path;
 use cmake::Config;
 
 fn main() {
+    let (cpp_compiler, cpplib) = if cfg!(target_os="linux") {
+        ("g++", "stdc++")
+    } else if cfg!(target_os="macos") {
+        ("clang++", "c++")
+    } else {
+        panic!("The system is not supported yet");
+    };
+
     let lib_name = "division_shader_compiler";
     let out_dir = env::var("OUT_DIR").unwrap();
     let install_dir = Path::new(&out_dir).join("lib");
@@ -11,7 +19,8 @@ fn main() {
     println!("cargo:rerun-if-changed=division_shader_compiler");
 
     println!("cargo:rustc-link-search=native={}", install_dir.to_str().unwrap());
-    println!("cargo:rustc-link-lib=dylib={}", "c++");
+
+    println!("cargo:rustc-link-lib=dylib={}", cpplib);
 
     let static_deps = [
         "division_shader_compiler",
@@ -35,5 +44,6 @@ fn main() {
     Config::new(lib_name)
         .target(lib_name)
         .out_dir(out_dir)
+        .define("CMAKE_CXX_COMPILER", cpp_compiler)
         .build();
 }
